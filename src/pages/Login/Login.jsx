2ba214b22fe0fe55fefbc95e9f20092c7f81b810
MUSHUBO <shubo3132@gmail.com>
2025-05-24 07:24:15 +0600

    Added the Loagin and Register page UI design done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Login = () => {
+
+    const handleLogIn = e => {
+        e.preventDefault();
+    }
+
     return (
-        <div>
-            login please
+        <div className='flex justify-center mx-6 my-20 md:my-25 lg:my-30'>
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
+                <h2 className='font-semibold text-3xl text-center'>Login now!</h2>
+                <form onSubmit={handleLogIn} className="card-body">
+                    <fieldset className="fieldset">
+                        {/* email */}
+                        <label className="label">Email</label>
+                        <input name='email' type="text"
+                            className="input" placeholder="Email" />
+
+                        {/* password */}
+                        <label className="label">Password</label>
+                        <input name='password' type="password"
+                            className="input" placeholder="Password" />
+
+                        <div><a className="link link-hover">Forgot password?</a></div>
+
+                        {/* button */}
+                        <button type='submit'
+                            className="btn btn-neutral mt-4">Login</button>
+
+                        <p className='flex gap-1 mt-5 justify-center font-semibold'>Don't have an account ?
+                            <Link to="/auth/register" className='text-blue-500 font-medium'>Register</Link>.
+                        </p>
+                    </fieldset>
+                </form>
+            </div>
         </div>
     );
 };
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Register = () => {
+
+    const handleRegister = e => {
+        e.preventDefault();
+    }
+
     return (
-        <div>
-            Register quick
+        <div className='flex justify-center mx-6 my-8'>
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
+                <h2 className='font-semibold text-3xl text-center'>Create Your Account!</h2>
+                <form onSubmit={handleRegister} className="card-body">
+                    <fieldset className="fieldset">
+                        {/* name */}
+                        <label className="label">Name</label>
+                        <input name='name' type="text"
+                            className="input" placeholder="Name" required />
+
+                        {/* photo url */}
+                        <label className="label">Photo URL</label>
+                        <input name='photo' type="text"
+                            className="input" placeholder="Photo URL" required />
+
+                        {/* email */}
+                        <label className="label">Email</label>
+                        <input name='email' type="email"
+                            className="input" placeholder="Email" required />
+
+                        {/* password */}
+                        <label className="label">Password</label>
+                        <input name='password' type="password"
+                            className="input" placeholder="Password" required />
+
+                        <div><a className="link link-hover">Forgot password?</a></div>
+
+                        {/* button */}
+                        <button type='submit' className="btn btn-neutral mt-4">Login</button>
+
+                        <p className='flex gap-1 mt-5 justify-center font-semibold'>Already have an account?
+                            <Link to="/auth/login" className='text-blue-500 font-medium'>Login Now</Link>.
+                        </p>
+                    </fieldset>
+                </form>
+            </div>
         </div>
     );
 };
